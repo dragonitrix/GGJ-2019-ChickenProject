606c09d65713259d55a75b7b3add5e99b331a483
ChickenMan86 <waltertheduuude@gmail.com>
2019-01-27 15:45:22 +0700

    Fixed Wolfbug

--- Assets/Wolf.cs
+++ Assets/Wolf.cs
@@ -179,6 +179,7 @@ public class Wolf : Object
                 break;
             case AttackTarget.home:
                 homeHealth.currentValue -= settings.attackDamage;
+                print("attackinghome");
                 break;
             default:
                 break;
